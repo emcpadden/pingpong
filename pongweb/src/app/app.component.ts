1931b55fb6e6d2c864338a4562af5b739afba37e
Ed McPadden <edward.mcpadden@tangoe.com>
2018-12-21 16:34:10 -0500

    unsubscribe when done

--- pongweb/src/app/app.component.ts
+++ pongweb/src/app/app.component.ts
@@ -1,33 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PingPongMessagingService } from './ping-pong-messaging.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  
   title = 'pongweb';
+  private connectionSubscription: Subscription = null;
+  private commandSubscription: Subscription = null;
+  private messageSubscription: Subscription = null;
 
   constructor(private pingPongMessagingService: PingPongMessagingService) {
   }
 
   ngOnInit() {
-    this.pingPongMessagingService.getConnection().subscribe(c => {
+    this.connectionSubscription = this.pingPongMessagingService.getConnection().subscribe(c => {
       let json = JSON.stringify(c);
       console.log(`PINGPONG CONNECTION: ${json}`);
     });
 
-    this.pingPongMessagingService.getCommand().subscribe(c => {
+    this.commandSubscription = this.pingPongMessagingService.getCommand().subscribe(c => {
       let json = JSON.stringify(c);
       console.log(`PINGPONG COMMAND: ${json}`);
     });
 
-    this.pingPongMessagingService.getMessage().subscribe(m => {
+    this.messageSubscription = this.pingPongMessagingService.getMessage().subscribe(m => {
       let json = JSON.stringify(m);
       console.log(`PINGPONG MESSAGE: ${json}`);
     });
 
     this.pingPongMessagingService.sendMessage('This is a test from Ping Service');
+  }
+
+  ngOnDestroy() {
+    if (!!this.connectionSubscription) {
+      this.connectionSubscription.unsubscribe();
+    }
+    if (!!this.commandSubscription) {
+      this.commandSubscription.unsubscribe();
+    }
+    if (!!this.messageSubscription) {
+      this.messageSubscription.unsubscribe();
+    }
   }
 }
